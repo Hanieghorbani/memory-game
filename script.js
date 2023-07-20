a973d753440fefaae82e44ad8a5561e49e61539d
hanie ghorbani <hnie.ghorbani@gmail.com>
2023-07-20 15:54:14 +0330

    80% complete

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const cells = document.querySelectorAll(".cell")
 const info = document.querySelector(".info")
+const remaningGameDiv = document.querySelector(".main-health")
+const reStartBtn = document.querySelector(".reStartBtn")
 const extArr = ["A", "B", "C", "D", 1, 2, 3, 4, 1, 2, 3, 4, "A", "B", "C", "D"]
 let numArr = []
 let count = 0
@@ -10,82 +12,135 @@ let cellSId
 let gameRemaining = 5
 let playing = true
 
-while (numArr.length < 16) {
-  let randNum = Math.floor(Math.random() * 16)
-
-  if (numArr.indexOf(randNum) === -1) {
-    numArr.push(randNum)
-  }
-}
-
+setCell()
 cells.forEach((cell, index) => {
   cell.firstElementChild.innerText = extArr[numArr[index]]
-  cell.addEventListener("click", (e) => {
-    if (count == 0 && playing) {
-      count = 1
-      e.target.firstElementChild.style.display = "block"
-      selectF = e.target.firstElementChild.innerText
-      cellFId = e.target.classList[1]
-    }
-    else if (count == 1 && playing) {
-      count = 2
-      cellSId = e.target.classList[1]
-      e.target.firstElementChild.style.display = "block"
-      selectS = e.target.firstElementChild.innerText
-
-      if (selectF == selectS) {
-        count = 0
-       if (
-        isWin(0) &&
-        isWin(1) &&
-        isWin(2) &&
-        isWin(3) &&
-        isWin(4) &&
-        isWin(5) &&
-        isWin(6) &&
-        isWin(7) &&
-        isWin(8) &&
-        isWin(9) &&
-        isWin(10) &&
-        isWin(11) &&
-        isWin(12) &&
-        isWin(13) &&
-        isWin(14) &&
-        isWin(15)
-        ) {
-        info.innerText = 'YOU WIN :)))'
-        playing = false
-       }
-      }
-      else{
-        info.innerText = `No Match ${gameRemaining}`
-        gameRemaining--
-        if (gameRemaining == 0) {
-          info.innerText = 'Game Over'
-          playing = false
-        }else{
-           cells.forEach(cell => {
-          if (cell.classList[1] == cellFId || cell.classList[1] == cellSId) {
-            setTimeout(() => {
-              cell.firstElementChild.style.display = 'none'
-              count = 0
-              info.innerText = ''
-            }, 1000);
-          }
-        })
-        }
-      }
-    }
-  })
+  cell.firstElementChild.style.display = 'none'
 })
 
-setTimeout(() => {
+
+function hidingAndStart() {
   cells.forEach((cell) => {
-    cell.firstElementChild.style.display = "none"
+    cell.firstElementChild.style.display = 'block'
   })
-}, 3000)
+  setTimeout(() => {
+    cells.forEach((cell) => {
+      cell.firstElementChild.style.display = "none"
+     
+        cell.addEventListener("click", (e) => {
+        console.log('click',playing);
+        if (count == 0 && playing) {
+          selectF = e.target.firstElementChild.innerText
+          cellFId = e.target.classList[1]
+          selectFS(e, 1)
+        } else if (count == 1 && playing) {
+          selectS = e.target.firstElementChild.innerText
+          cellSId = e.target.classList[1]
+          selectFS(e, 2)
+          //for true select
+          if (selectF == selectS) {
+            count = 0
+            //for win
+            if (
+              isWin(0) &&
+              isWin(1) &&
+              isWin(2) &&
+              isWin(3) &&
+              isWin(4) &&
+              isWin(5) &&
+              isWin(6) &&
+              isWin(7) &&
+              isWin(8) &&
+              isWin(9) &&
+              isWin(10) &&
+              isWin(11) &&
+              isWin(12) &&
+              isWin(13) &&
+              isWin(14) &&
+              isWin(15)
+            ) {
+              info.innerText = "YOU WIN :)))"
+              playing = false
+              reStartBtn.style.display = "block"
+            }
+          }
+          //for false select
+          else {
+            console.log("false select")
+            gameRemaining--
+            info.innerText = `No Match`
+            remaningGameDiv.style.width = `${gameRemaining * 20}%`
+            if (gameRemaining < 3) {
+              remaningGameDiv.style.backgroundColor = "rgb(235, 81, 81)"
+            }
+            if (gameRemaining != 0) {
+              cells.forEach((cell) => {
+                if (
+                  cell.classList[1] == cellFId ||
+                  cell.classList[1] == cellSId
+                ) {
+                  setTimeout(() => {
+                    cell.firstElementChild.style.display = "none"
+                    count = 0
+                    info.innerText = ""
+                  }, 1000)
+                }
+              })
+            }
+            //game over
+            else {
+              info.innerText = "Game Over"
+              reStartBtn.innerText = 'RESTART'
+              reStartBtn.style.display = "block"
+              playing = false
+            }
+          }
+        }
+      })
+      
+    })
+  }, 3000)
+}
 
 function isWin(i) {
+  return cells[i].firstElementChild.style.display == "block"
+}
+function selectFS(e, cntNum) {
+  count = cntNum
+  e.target.firstElementChild.style.display = "block"
+}
+
+reStartBtn.addEventListener("click", ()=>{
   
-  return cells[i].firstElementChild.style.display == 'block'
+  if (reStartBtn.innerText == 'Start') {
+    hidingAndStart()
+    reStartBtn.style.display = 'none'
+  }else{
+    reStarting()
+  }
+})
+
+function reStarting() {
+  reStartBtn.style.display = "none"
+  remaningGameDiv.style.backgroundColor = "rgb(80, 182, 80)"
+  remaningGameDiv.style.width = "100%"
+  info.innerText = ""
+  gameRemaining = 5
+  count = 0
+  playing = true
+  numArr = []
+  setCell()
+  cells.forEach((cell, index) => {
+    cell.firstElementChild.innerText = extArr[numArr[index]]
+  })
+  hidingAndStart()
+}
+
+function setCell() {
+  while (numArr.length < 16) {
+    let randNum = Math.floor(Math.random() * 16)
+    if (numArr.indexOf(randNum) === -1) {
+      numArr.push(randNum)
+    }
+  }
 }
